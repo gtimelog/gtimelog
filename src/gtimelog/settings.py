5b5dc26302f25d3569c59148fd36a3f4a7968125
Standa Mitrega <standa31415@gmail.com>
2014-01-14 19:02:39 +0200

    WIP: Add subcategories to reports
    This is patch
    https://launchpadlibrarian.net/161449651/gtimelog-0.9.1__subcats_withdate__20140102.diff
    from https://bugs.launchpad.net/gtimelog/+bug/1185041, applied with git
    apply, for review purposes.

--- src/gtimelog/settings.py
+++ src/gtimelog/settings.py
@@ -51,6 +51,19 @@ class Settings(object):
     start_in_tray = False
 
     report_style = 'plain'
+    """If True then timestamp of the event will be present in categorized report"""
+    report_categorized_withdate = False
+    
+    """If True then treat categories with 'subcategories_separator' as multi-level categories"""
+    subcategories_enabled = False
+    """Separator for levels in multi-level categories"""
+    subcategories_separator = '_'
+    
+    timelog_filename = None
+    timelog_filename_def = 'timelog.txt'
+    config_filename = None
+    config_filename_def = 'gtimelogrc'
+    
 
     def check_legacy_config(self):
         envar_home = os.environ.get('GTIMELOG_HOME')
@@ -76,11 +89,44 @@ class Settings(object):
         xdg = os.environ.get('XDG_DATA_HOME') or '~/.local/share'
         return os.path.join(os.path.expanduser(xdg), 'gtimelog')
 
+    def set_config_file(self, filename = None):
+        """Setter mainly for testing purposes.
+        
+        Set filename if not set or if filename is provided"""
+        if (self.config_filename is None) or (filename is not None):
+            if filename is None:
+                self.config_filename = os.path.join(self.get_config_dir(), self.config_filename_def)
+            else:
+                self.config_filename = filename
+                
     def get_config_file(self):
-        return os.path.join(self.get_config_dir(), 'gtimelogrc')
+        if self.config_filename is None:
+            self.set_config_file()
+        return self.config_filename
+
+    def set_timelog_file(self, filename = None):
+        """Setter mainly for testing purposes.
+        
+        Set filename if not set or if filename is provided"""
+        if (self.timelog_filename is None) or (filename is not None):
+            if filename is None:
+                self.timelog_filename = os.path.join(self.get_data_dir(), self.timelog_filename_def)
+            else:
+                self.timelog_filename = filename
 
     def get_timelog_file(self):
-        return os.path.join(self.get_data_dir(), 'timelog.txt')
+        if self.timelog_filename is None:
+            self.set_timelog_file()
+        return self.timelog_filename
+
+    def set_subcategories_separator(self, separator):
+        self.subcategories_separator = separator[0]
+    
+    def set_enable_subcategories(self, enable):
+            self.subcategories_enabled = enable
+
+    def set_enable_report_categorized_withdate(self, enable):
+            self.report_categorized_withdate = enable
 
     def _config(self):
         config = RawConfigParser()
@@ -108,7 +154,14 @@ class Settings(object):
         config.set('gtimelog', 'prefer_old_tray_icon',
                    str(self.prefer_old_tray_icon))
         config.set('gtimelog', 'report_style', str(self.report_style))
+        config.set('gtimelog', 'report_categorized_withdate', 
+                   str(self.report_categorized_withdate))
         config.set('gtimelog', 'start_in_tray', str(self.start_in_tray))
+        config.set('gtimelog', 'subcategories_enabled', 
+                   str(self.subcategories_enabled))
+        config.set('gtimelog', 'subcategories_separator', 
+                   self.set_subcategories_separator( 
+                       str(self.subcategories_separator)))
         return config
 
     if PY3:
@@ -144,7 +197,13 @@ class Settings(object):
         self.prefer_old_tray_icon = config.getboolean('gtimelog',
                                                       'prefer_old_tray_icon')
         self.report_style = config.get('gtimelog', 'report_style')
+        self.report_categorized_withdate = config.getboolean('gtimelog', 
+                                                             'report_categorized_withdate')
         self.start_in_tray = config.getboolean('gtimelog', 'start_in_tray')
+        self.subcategories_enabled = config.getboolean('gtimelog', 
+                                                'subcategories_enabled' ) 
+        self.subcategories_separator = config.get('gtimelog', 
+                                                  'subcategories_separator') 
 
     def save(self, filename):
         config = self._config()
