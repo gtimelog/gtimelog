gtimelog/gtimelog

commit ea1e19c8a857a989edfaa495c2fde546d67f93ce

Speed up entry addition (#21)
When I added auto-reloading by polling the timelog mtime every second I
somehow forgot that adding a new entry to the end of the file also
*changes the mtime*.  And so the next tick will reload *everything*.
Which takes a while, when your timelog.txt is 20 thousand lines ling.

src/gtimelog/timelog.py
@@ -796,6 +796,7 @@ class TimeLog(object):
             f.write('\n')
         f.write(line + '\n')
         f.close()
+        self.last_mtime = self.get_mtime()
 
     def append(self, entry, now=None):
         """Append a new entry to the time log."""

commit d4be5665110d2417324d8b784eb6c5d37f215778

Make entry addition fast again
Fixes a regression in #21 that occurred after the fix for #28

src/gtimelog/timelog.py
@@ -900,7 +900,9 @@ class TimeLog(object):
         self.window.items.append((now, entry))
         line = '%s: %s' % (now.strftime("%Y-%m-%d %H:%M"), entry)
         self.raw_append(line)
-        self._cache.clear()
+        for (min, max), cached in self._cache.iteritems():
+            if cached is not self.window and min <= now < max:
+                cached.items.append((now, entry))
 
     def valid_time(self, time):
         if time > datetime.datetime.now():