606224e94675d953e6803d391bab6970610f4617
Marius Gedminas <marius@gedmin.as>
2015-09-03 12:50:10 +0300

    Move the <menu> definition out of the .ui file
    Because Glade doesn't support GMenu and strips it out when saving:
    https://bugzilla.gnome.org/show_bug.cgi?id=667970

--- mockup.py
+++ mockup.py
@@ -6,8 +6,22 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 ui_file = 'src/gtimelog/experiment.ui'
+menu_def = '''
+<interface>
+  <menu id="window_menu">
+    <item>
+      <attribute name="label">Edit timelog.txt</attribute>
+    </item>
+    <item>
+      <attribute name="label">Quit</attribute>
+    </item>
+  </menu>
+</interface>
+'''
 
 builder = Gtk.Builder.new_from_file(ui_file)
+builder.add_from_string(menu_def)
+builder.get_object('menu_button').set_menu_model(builder.get_object('window_menu'))
 
 if __name__ == '__main__':
     app = Gtk.Application(application_id='lt.pov.mg.gtimelog_mockup')
