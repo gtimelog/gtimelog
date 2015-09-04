5bd117c17213dda54c9179217a58b7a34dc6d51f
Marius Gedminas <marius@gedmin.as>
2015-09-04 09:42:04 +0300

    Refactor the code and UI files
    Make the About dialog work.

--- mockup.py
+++ mockup.py
@@ -2,144 +2,106 @@
 from __future__ import print_function
 
 import signal
+import sys
 
 import gi
 gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk, Gio, GLib
 
-from gi.repository import Gtk, Gio, GLib
 
-ui_file = 'src/gtimelog/experiment.ui'
-menu_def = '''
-<interface>
-  <menu id="app_menu">
-    <section>
-      <item>
-        <attribute name="label">Help</attribute>
-        <attribute name="action">app.help</attribute>
-      </item>
-      <item>
-        <attribute name="label">About</attribute>
-        <attribute name="action">app.about</attribute>
-      </item>
-    </section>
-    <section>
-      <item>
-        <attribute name="label">Quit</attribute>
-        <attribute name="action">app.quit</attribute>
-        <attribute name="accel">&lt;Primary&gt;Q</attribute>
-      </item>
-    </section>
-  </menu>
-  <menu id="window_menu">
-    <section>
-      <item>
-        <attribute name="label">Edit timelog.txt</attribute>
-        <attribute name="action">app.edit-log</attribute>
-      </item>
-    </section>
-    <section>
-      <item>
-        <attribute name="label">Send report...</attribute>
-        <attribute name="action">win.send-report</attribute>
-      </item>
-    </section>
-  </menu>
-  <menu id="view_menu">
-    <section>
-      <item>
-        <attribute name="label">Detail level</attribute>
-        <attribute name="action">disabled</attribute>
-      </item>
-      <item>
-        <attribute name="label">Chronological</attribute>
-        <attribute name="action">win.detail-level</attribute>
-        <attribute name="target">chronological</attribute>
-      </item>
-      <item>
-        <attribute name="label">Grouped</attribute>
-        <attribute name="action">win.detail-level</attribute>
-        <attribute name="target">grouped</attribute>
-      </item>
-      <item>
-        <attribute name="label">Summary</attribute>
-        <attribute name="action">win.detail-level</attribute>
-        <attribute name="target">summary</attribute>
-      </item>
-    </section>
-    <section>
-      <item>
-        <attribute name="label">Time range</attribute>
-        <attribute name="action">disabled</attribute>
-      </item>
-      <item>
-        <attribute name="label">Day</attribute>
-        <attribute name="action">win.time-range</attribute>
-        <attribute name="target">day</attribute>
-      </item>
-      <item>
-        <attribute name="label">Week</attribute>
-        <attribute name="action">win.time-range</attribute>
-        <attribute name="target">week</attribute>
-      </item>
-      <item>
-        <attribute name="label">Month</attribute>
-        <attribute name="action">win.time-range</attribute>
-        <attribute name="target">month</attribute>
-      </item>
-      <item>
-        <attribute name="label">Custom...</attribute>
-        <attribute name="action">win.time-range</attribute>
-        <attribute name="target">custom</attribute>
-      </item>
-    </section>
-  </menu>
-</interface>
-'''
+HELP_URL = 'https://mg.pov.lt/gtimelog'
+UI_FILE = 'src/gtimelog/experiment.ui'
+ABOUT_DIALOG_UI_FILE = 'src/gtimelog/about.ui'
+MENUS_UI_FILE = 'src/gtimelog/menus.ui'
 
-builder = Gtk.Builder.new_from_file(ui_file)
-builder.add_from_string(menu_def)
-builder.get_object('menu_button').set_menu_model(builder.get_object('window_menu'))
-builder.get_object('view_button').set_menu_model(builder.get_object('view_menu'))
+
+class Application(Gtk.Application):
+
+    def __init__(self):
+        super(Application, self).__init__(application_id='lt.pov.mg.gtimelog_mockup')
+        GLib.set_application_name("Time Log")
+        GLib.set_prgname('gtimelog')
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+        builder = Gtk.Builder.new_from_file(MENUS_UI_FILE)
+        self.set_app_menu(builder.get_object('app_menu'))
+
+        for action_name in ['help', 'about', 'quit', 'edit-log']:
+            action = Gio.SimpleAction.new(action_name, None)
+            action.connect('activate', getattr(self, 'on_' + action_name.replace('-', '_')))
+            self.add_action(action)
+
+        self.set_accels_for_action("win.detail-level('chronological')", ["<Alt>1"])
+        self.set_accels_for_action("win.detail-level('grouped')", ["<Alt>2"])
+        self.set_accels_for_action("win.detail-level('summary')", ["<Alt>3"])
+        self.set_accels_for_action("win.show-task-pane", ["F9"])
+        self.set_accels_for_action("win.go-back", ["<Alt>Left"])
+        self.set_accels_for_action("win.go-forward", ["<Alt>Right"])
+        self.set_accels_for_action("win.go-home", ["<Alt>Home"])
+        self.set_accels_for_action("app.edit-log", ["<Primary>E"])
+        self.set_accels_for_action("app.quit", ["<Primary>Q"])
+        self.set_accels_for_action("win.send-report", ["<Primary>D"])
+
+    def on_quit(self, action, parameter):
+        self.quit()
+
+    def on_edit_log(self, action, parameter):
+        print("Pretend-editing timelog.txt")
+
+    def on_help(self, action, parameter):
+        Gtk.show_uri(None, HELP_URL, Gdk.CURRENT_TIME)
+
+    def on_about(self, action, parameter):
+        builder = Gtk.Builder.new_from_file(ABOUT_DIALOG_UI_FILE)
+        about_dialog = builder.get_object('about_dialog')
+        about_dialog.set_transient_for(self.get_active_window())
+        about_dialog.connect("response", lambda *args: about_dialog.destroy())
+        about_dialog.show()
+
+    def do_activate(self):
+        builder = Gtk.Builder.new_from_file(UI_FILE)
+        builder.add_from_file(MENUS_UI_FILE)
+        window = builder.get_object('main_window')
+        window.__class__ = Window
+        window._init(builder)
+        self.add_window(window)
+        window.show()
+
+
+class Window(Gtk.ApplicationWindow):
+
+    def _init(self, builder):
+        builder.get_object('menu_button').set_menu_model(builder.get_object('window_menu'))
+        builder.get_object('view_button').set_menu_model(builder.get_object('view_menu'))
+
+        detail_level = Gio.SimpleAction.new_stateful("detail-level", GLib.VariantType.new("s"), GLib.Variant("s", "chronological"))
+        self.add_action(detail_level)
+
+        time_range = Gio.SimpleAction.new_stateful("time-range", GLib.VariantType.new("s"), GLib.Variant("s", "day"))
+        self.add_action(time_range)
+
+        task_pane = Gio.PropertyAction.new("show-task-pane", builder.get_object("task_pane"), "visible")
+        self.add_action(task_pane)
+
+        go_back = Gio.SimpleAction.new("go-back", None)
+        self.add_action(go_back)
+
+        go_forward = Gio.SimpleAction.new("go-forward", None)
+        go_forward.set_enabled(False)
+        self.add_action(go_forward)
+
+        go_home = Gio.SimpleAction.new("go-home", None)
+        go_home.set_enabled(False)
+        self.add_action(go_home)
+
+
+def main():
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    app = Application()
+    sys.exit(app.run(sys.argv))
 
 
 if __name__ == '__main__':
-    app = Gtk.Application(application_id='lt.pov.mg.gtimelog_mockup')
-    def _startup(app):
-        app.set_app_menu(builder.get_object('app_menu'))
-        quit = Gio.SimpleAction.new("quit", None)
-        quit.connect('activate', lambda *args: app.quit())
-        app.add_action(quit)
-        edit = Gio.SimpleAction.new("edit-log", None)
-        edit.connect('activate', lambda *args: print("Pretend-editing timelog.txt"))
-        app.add_action(edit)
-        app.set_accels_for_action("win.detail-level('chronological')", ["<Alt>1"])
-        app.set_accels_for_action("win.detail-level('grouped')", ["<Alt>2"])
-        app.set_accels_for_action("win.detail-level('summary')", ["<Alt>3"])
-        app.set_accels_for_action("win.show-task-pane", ["F9"])
-        app.set_accels_for_action("win.go-back", ["<Alt>Left"])
-        app.set_accels_for_action("win.go-forward", ["<Alt>Right"])
-        app.set_accels_for_action("win.go-home", ["<Alt>Home"])
-        app.set_accels_for_action("app.edit-log", ["<Primary>E"])
-        app.set_accels_for_action("win.send-report", ["<Primary>D"])
-    app.connect('startup', _startup)
-    def _activate(app):
-        window = builder.get_object('main_window')
-        detail_level = Gio.SimpleAction.new_stateful("detail-level", GLib.VariantType.new("s"), GLib.Variant("s", "chronological"))
-        window.add_action(detail_level)
-        time_range = Gio.SimpleAction.new_stateful("time-range", GLib.VariantType.new("s"), GLib.Variant("s", "day"))
-        window.add_action(time_range)
-        task_pane = Gio.PropertyAction.new("show-task-pane", builder.get_object("task_pane"), "visible")
-        window.add_action(task_pane)
-        go_back = Gio.SimpleAction.new("go-back", None)
-        window.add_action(go_back)
-        go_forward = Gio.SimpleAction.new("go-forward", None)
-        go_forward.set_enabled(False)
-        window.add_action(go_forward)
-        go_home = Gio.SimpleAction.new("go-home", None)
-        go_home.set_enabled(False)
-        window.add_action(go_home)
-        app.add_window(window)
-        window.show()
-    app.connect('activate', _activate)
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    app.run()
+    main()
