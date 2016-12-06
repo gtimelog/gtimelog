154ca34adf10dd299fc927078aec9466c756a440
Laurynas Speičys <laurynas@pov.lt>
2016-12-06 10:30:11 +0200

    Accomodate one more format of time

--- scripts/sum.py
+++ scripts/sum.py
@@ -5,15 +5,18 @@ import re
 
 time_rx = re.compile(r'(\d+) hours?,? (\d+) min$'
                      r'|(\d+) hours?$'
-                     r'|(\d+) min$')
+                     r'|(\d+) min$'
+                     r'|(\d+) h (\d+) min$'
+                     r'|(\d+):(\d+)$'
+                     )
 
 
 def parse_time(s):
     m = time_rx.match(s)
     if not m:
         return None
-    h1, m1, h2, m2 = m.groups()
-    return int(h1 or h2 or '0') * 60 + int(m1 or m2 or '0')
+    h1, m1, h2, m2, h3, m3, h4, m4 = m.groups()
+    return int(h1 or h2 or h3 or h4 or '0') * 60 + int(m1 or m2 or m3 or m4 or '0')
 
 
 def format_time(t):
