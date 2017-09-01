a6628c3ff1e19807dbbeff4ab915117ea20d9cd3
Marius Gedminas <marius@gedmin.as>
2017-09-01 17:09:38 +0300

    Claim Python 3.6 support

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         # 2.6 might work, but I can't test it myself -- recent
         # python-gobject versions dropped support for Python 2.6
         'Topic :: Office/Business',
