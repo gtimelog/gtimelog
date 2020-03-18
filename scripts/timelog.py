76bb2223a3217c9992db4a9d900352baed18ced6
Roman Kamyk <rkj@google.com>
2020-03-18 08:23:18 -0700

    Add flag support to timelog.py, to allow writing to a different file.
    Scripts are great when working through SSH instead of locally.

--- scripts/timelog.py
+++ scripts/timelog.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python
 import datetime
 import readline  # noqa: make raw_input() friendlier
+import getopt
+import sys
+import os
 
-
-f = open("timelog.txt", "a")
-print >> f
-f.close()
-
-while True:
-    try:
-        what = raw_input("> ")
-    except EOFError:
-        print
-        break
-    ts = datetime.datetime.now()
-    line = "%s: %s" % (ts.strftime("%Y-%m-%d %H:%M"), what)
-    print line
-    f = open("timelog.txt", "a")
-    print >> f, line
+def main(argv=sys.argv):
+  filename = 'timelog.txt'
+  printnewline = False
+  opts, args = getopt.getopt(argv[1:], 'hf:', ['help'])
+  for k, v in opts:
+    if k == '-f':
+      filename = v
+    if k == '-n':
+      printnewline = True
+    if k == '-h':
+      print("todo")
+      return
+  if len(args) > 1:
+    print >> sys.stderr, "too many arguments"
+    return
+  if printnewline:
+    f = open(filename, "a")
+    print >> f
     f.close()
+
+  while True:
+      try:
+          what = raw_input("> ")
+      except EOFError:
+          print
+          break
+      ts = datetime.datetime.now()
+      line = "%s: %s" % (ts.strftime("%Y-%m-%d %H:%M"), what)
+      print line
+      f = open(filename, "a")
+      print >> f, line
+      f.close()
+
+if __name__ == '__main__':
+    main()
