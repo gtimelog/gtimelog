83ef59739c3d524b945ccda8d516df359c1970aa
StephaneMangin <stephane.mangin@orinoko.fr>
2020-10-07 19:40:41 +0200

    TimeWindow test renamed

--- src/gtimelog/tests/__init__.py
+++ src/gtimelog/tests/__init__.py
@@ -1,7 +1,7 @@
 """Tests for gtimelog"""
 import unittest
 
-from gtimelog.tests.core import test_utils, test_settings, test_timelog,\
+from gtimelog.tests.core import test_utils, test_settings, test_time,\
     test_report_record, test_exports, test_reports
 from gtimelog.tests.modules import test_email
 
@@ -10,7 +10,7 @@ def test_suite():
     return unittest.TestSuite([
         test_utils.test_suite(),
         test_settings.test_suite(),
-        test_timelog.test_suite(),
+        test_time.test_suite(),
         test_report_record.test_suite(),
         test_reports.test_suite(),
         test_exports.test_suite(),
