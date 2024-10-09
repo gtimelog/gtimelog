53ae4fa5e68b64b1ab38384524a04f59a4b1d179
Marius Gedminas <marius@gedmin.as>
2024-10-09 13:26:01 +0300

    Make sum.py accept alternative time format and support -decimal output
    Some people send reports that say
    
       task task HH:MM
    
    and some people need a total number of hours (with a fraction), not
    hours and minutes.

--- scripts/sum.py
+++ scripts/sum.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python
-import sys
+import argparse
 import re
+import sys
 
 
 time_rx = re.compile(r'(\d+) hours?,? (\d+) min$'
                      r'|(\d+) hours?$'
-                     r'|(\d+) min$')
+                     r'|(\d+) min$'
+                     r'|(\d\d+):(\d\d)$')
 
 
 def parse_time(s):
     m = time_rx.match(s)
     if not m:
         return None
-    h1, m1, h2, m2 = m.groups()
-    return int(h1 or h2 or '0') * 60 + int(m1 or m2 or '0')
+    h1, m1, h2, m2, h3, m3 = m.groups()
+    return int(h1 or h2 or h3 or '0') * 60 + int(m1 or m2 or m3 or '0')
+
+
+def parse_time_line(line):
+    if '  ' in line:
+        return parse_time(line.rpartition('  ')[-1])
+    elif ' ' in line:
+        return parse_time(line.rpartition(' ')[-1])
+    else:
+        return None
 
 
 def format_time(t):
@@ -26,14 +37,44 @@ def format_time(t):
         return '%d min' % m
 
 
-total = 0
-for line in sys.stdin:
-    if '  ' not in line:
-        continue
-    time = parse_time(line.split('  ')[-1].strip())
-    if time is None:
-        continue
-    print(line.rstrip())
-    total += time
+parser = argparse.ArgumentParser(description="sum time entries")
+parser.add_argument(
+    "-d", "--decimal", action="store_true",
+    help="output decimal hours (12.3) instead of hours and minutes")
+parser.add_argument(
+    "--test", action="store_true",
+    help=argparse.SUPPRESS)
 
-print("** Total: %s" % format_time(total))
+
+def main():
+    args = parser.parse_args()
+    if args.test:
+        test_parse_time()
+        sys.exit()
+
+    total = 0
+    for line in sys.stdin:
+        line = line.rstrip()
+        time = parse_time_line(line)
+        if time is None:
+            continue
+        print(line)
+        total += time
+
+    if args.decimal:
+        print("** Total: %.2f hours" % total)
+    else:
+        print("** Total: %s" % format_time(total))
+
+
+def test_parse_time():
+    assert parse_time('1 hour 42 min') == 102
+    assert parse_time('1 hour') == 60
+    assert parse_time('42 min') == 42
+    assert parse_time('01:42') == 102
+    assert parse_time_line('task task 01:42') == 102
+    assert parse_time_line('task task  1 hour 42 min') == 102
+
+
+if __name__ == "__main__":
+    main()
